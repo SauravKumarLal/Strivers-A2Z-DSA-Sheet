c7cefd0e3eaa1d72670a53e5523127ad7d774fe5
Saurav Kumar Lal <94238309+SauravKumarLal@users.noreply.github.com>
2023-08-11 23:16:26 +0530

    Create pattern18.cpp

---
+++ Step 1 : Learn the basics/Step 1.2 : Build-up Logical Thinking/pattern18.cpp
@@ -0,0 +1,42 @@
+/*
+E
+E D
+E D C
+E D C B
+E D C B A
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            char ch='A'+n-1;
+            for(int j=0;j<=i;j++){
+                cout<<ch<<" ";
+                ch--;
+            }cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
